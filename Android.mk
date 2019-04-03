978cbd26555865eb644385b417cc98b7256ff5cc
Erik Castricum <git@cas-online.nl>
2019-04-03 11:43:23 +0200

    introduce caf buildguard and update paths
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I1d7daa6ca836ea4797a73946ca690442b94dbad7

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),msm8996-caf)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
@@ -20,4 +21,5 @@ ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
 endif
 
+endif
 endif
