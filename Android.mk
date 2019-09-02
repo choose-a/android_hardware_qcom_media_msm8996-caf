7932125b305cc860341edc98af3aa366a655211c
Erik Castricum <git@cas-online.nl>
2019-09-02 10:34:41 +0200

    remove media root guard
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I572798b31fbff363fbbb7e7a77309394c88d5294

--- Android.mk
+++ Android.mk
@@ -1,12 +1,9 @@
 ifeq ($(TARGET_QCOM_MEDIA_VARIANT),msm8996-caf)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
-#Compile these for all targets under QCOM_BOARD_PLATFORMS list.
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 include $(QCOM_MEDIA_ROOT)/libaac/Android.mk
-endif
 
 ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
